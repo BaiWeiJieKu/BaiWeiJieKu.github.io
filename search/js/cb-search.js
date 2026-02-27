57eb1f46695826499615ad20ea01d43720649530
BaiWeiJieKu <2534964002@qq.com>
2026-02-27 14:32:37 +0800

    改布局

--- search/js/cb-search.js
+++ search/js/cb-search.js
@@ -1,15 +1,16 @@
-﻿// 获取搜索框、搜索按钮、清空搜索、结果输出对应的元素
+// 获取搜索框、搜索按钮、清空搜索、结果输出对应的元素
 var searchBtn = document.querySelector('.search-start');
 var searchClear = document.querySelector('.search-clear');
 var searchInput = document.querySelector('.search-input');
 var searchResults = document.querySelector('.search-results');
 
-// 申明保存文章的标题、链接、内容的数组变量
+// 申明保存文章的标题、链接、内容、标签/分类的数组变量
 var searchValue = '',
     arrItems = [],
     arrContents = [],
     arrLinks = [],
     arrTitles = [],
+    arrCategories = [],
     arrResults = [],
     indexItem = [];
 var tmpDiv = document.createElement('div');
@@ -21,12 +22,22 @@ xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
         xml = xhr.responseXML;
         arrItems = xml.getElementsByTagName('item');
-        
-        // 遍历并保存所有文章对应的标题、链接、内容到对应的数组中
+
+        // 遍历并保存所有文章对应的标题、链接、内容、标签/分类到对应的数组中
         for (i = 0; i < arrItems.length; i++) {
             arrContents[i] = arrItems[i].getElementsByTagName('description')[0].childNodes[0].nodeValue;
             arrLinks[i] = arrItems[i].getElementsByTagName('link')[0].childNodes[0].nodeValue.replace(/\s+/g, '');
             arrTitles[i] = arrItems[i].getElementsByTagName('title')[0].childNodes[0].nodeValue;
+
+            // 收集 <category> 中的标签与分类，合并为字符串，便于检索
+            var cateNodeList = arrItems[i].getElementsByTagName('category');
+            var cateTextArr = [];
+            for (var j = 0; j < cateNodeList.length; j++) {
+                if (cateNodeList[j].childNodes[0]) {
+                    cateTextArr.push(cateNodeList[j].childNodes[0].nodeValue);
+                }
+            }
+            arrCategories[i] = cateTextArr.join(' ');
         }
     }
 }
@@ -68,7 +79,7 @@ function searchConfirm() {
         // 合法输入值的情况
         searchInit();
         searchValue = searchInput.value;
-        searchMatching(arrTitles, searchValue);
+        searchMatching(searchValue);
     }
 }
 
@@ -81,18 +92,40 @@ function searchInit() {
     searchClear.style.display = 'block';
 }
 
-function searchMatching(arr, input) {
+function searchMatching(input) {
 
-
-    // 在所有文章内容中匹配查询值
-    for (i = 0; i < arr.length; i++) {
-        if (arr[i].search(input) != -1) {
+    // 在标题、摘要和标签/分类中综合匹配查询值
+    for (i = 0; i < arrTitles.length; i++) {
+        var composite = (arrTitles[i] + ' ' + arrContents[i] + ' ' + arrCategories[i]).toLowerCase();
+        var keyword = input.toLowerCase();
+        var hitIndex = composite.indexOf(keyword);
+        if (hitIndex !== -1) {
             indexItem.push(i);
-            var indexContent = arr[i].search(input);
-            var l = input.length;
-            
-            // 将匹配到内容的地方进行黄色标记，并包括周围一定数量的文本
-            arrResults.push('<mark>' + arr[i].slice(indexContent, indexContent + l) + '</mark>');
+
+            // 优先在标题中截取高亮片段
+            var displayText = arrTitles[i];
+            var titleIndex = arrTitles[i].toLowerCase().indexOf(keyword);
+            if (titleIndex !== -1) {
+                displayText =
+                    arrTitles[i].slice(0, titleIndex) +
+                    '<mark>' +
+                    arrTitles[i].slice(titleIndex, titleIndex + input.length) +
+                    '</mark>' +
+                    arrTitles[i].slice(titleIndex + input.length);
+            } else {
+                // 若标题未命中，则在摘要或标签中截取一小段上下文
+                var contextSource = (arrContents[i] + ' ' + arrCategories[i]);
+                var contextIndex = contextSource.toLowerCase().indexOf(keyword);
+                var start = Math.max(0, contextIndex - 20);
+                var end = Math.min(contextSource.length, contextIndex + input.length + 20);
+                var snippet = contextSource.slice(start, end);
+                snippet = snippet.replace(new RegExp(input, 'ig'), function (match) {
+                    return '<mark>' + match + '</mark>';
+                });
+                displayText = snippet;
+            }
+
+            arrResults.push(displayText);
         }
     }
 
